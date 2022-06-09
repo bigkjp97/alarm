49d07af6046727f126ceba7039d60c2c26cf5e20
pengkunzhou <pengkunzhou@gci-china.com>
2022-06-09 21:07:51 +0800

    send parameter into redis connection

--- pkg/conn/redis.go
+++ pkg/conn/redis.go
@@ -10,21 +10,22 @@ import (
 )
 
 func NewRedisConn(c *utils.Config) *redis.Pool {
-	fmt.Printf("connection to redis ...")
 	fmt.Printf(c.App.Log_file)
-	// go引用结构体指针
 
+	// go引用结构体指针
 	return &redis.Pool{
 		MaxIdle:     c.Redis.Max_idle,                   // 最大的空闲连接数
 		IdleTimeout: c.Redis.Idle_timeout * time.Second, // 最大的空闲连接等待时间
 
 		// 建立连接
 		Dial: func() (redis.Conn, error) {
+			fmt.Printf("connection to redis ...")
 			conn, err := redis.Dial(c.Redis.Network, c.Redis.Address)
 			if err != nil {
 				// log.Fatalf("redis connection error %v", err)
 				fmt.Printf("fail!")
 			}
+			fmt.Printf(c.Redis.Auth)
 			if c.Redis.Auth != "" {
 				_, err := conn.Do("AUTH", c.Redis.Auth)
 				if err != nil {
